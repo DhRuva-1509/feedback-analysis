476b35ca54939500340886e36ad2948495d1c4f9
Dhruva Patil <dhruvasp.ca@gmail.com>
2025-06-24 00:19:18 -0300

    Made trigger in Lambda with S3

--- env/dev/s3/main.tf
+++ env/dev/s3/main.tf
@@ -4,3 +4,23 @@ module "s3_uploads" {
   environment = var.environment
   bucket_name = var.bucket_name
 }
+
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = module.s3_uploads.bucket_arn
+}
+
+resource "aws_s3_bucket_notification" "trigger_analyze" {
+  bucket = module.s3_uploads.bucket_name
+
+  lambda_function {
+    lambda_function_arn = var.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "incoming/"
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
+}
--- env/dev/s3/variables.tf
+++ env/dev/s3/variables.tf
@@ -17,3 +17,11 @@ variable "aws_region" {
 variable "aws_profile" {
   type = string
 }
+
+variable "lambda_function_name" {
+  type = string
+}
+
+variable "lambda_function_arn" {
+  type = string
+}
